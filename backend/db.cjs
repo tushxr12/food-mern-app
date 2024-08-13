28428da5877ab76a0217b78a2dafc91826152d68
tushxr12 <tusharforcourses@gmail.com>
2024-08-13 23:42:27 +0530

    add try catch for mongodb connection

--- backend/db.cjs
+++ backend/db.cjs
@@ -5,9 +5,17 @@ dotenv.config();
 const URI = process.env.MongoDBURI;
 
 //connect to MongoDB
-const connectDB = () => {
-  mongoose.connect(URI);
-  console.log("Database connected successfully");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("Database (food-mern-app) connected successfully");
+
+    const fetched_data = await mongoose.connection.db.collection("food-items");
+    const data = await fetched_data.find({}).toArray();
+    // console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = connectDB;
