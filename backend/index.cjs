8be9032eb90446eadc419c4e34abe20e39f93ff8
tushxr12 <tusharforcourses@gmail.com>
2024-08-13 23:42:52 +0530

    add : call route in index file

--- backend/index.cjs
+++ backend/index.cjs
@@ -12,6 +12,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api", require("./routes/createUser.cjs"));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
