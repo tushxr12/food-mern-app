60b341a580ff188af5f0fb3bb233bf3cbddeba67
tushxr12 <tusharforcourses@gmail.com>
2024-08-16 14:55:20 +0530

    create endpoint for user login

--- backend/routes/createUser.cjs
+++ backend/routes/createUser.cjs
@@ -30,4 +30,36 @@ router.post(
   }
 );
 
+router.post(
+  "/loginuser",
+  [
+    body("email", "Given email is not in proper format.").isEmail(),
+    body("password", "Password length is too short.").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let email = req.body.email;
+    try {
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res
+          .status(400)
+          .json({ errors: "Email/Password is not correct." });
+      }
+      if (req.body.password !== userData.password) {
+        return res
+          .status(400)
+          .json({ errors: "Email/Password is not correct." });
+      }
+      res.json({ success: true });
+    } catch (error) {
+      console.log("Error : " + error.message);
+      res.json({ success: false });
+    }
+  }
+);
+
 module.exports = router;
