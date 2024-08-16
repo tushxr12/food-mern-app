11b3f3bed94c33a264c59f18e59b6a54a355d99c
tushxr12 <tusharforcourses@gmail.com>
2024-08-16 13:56:15 +0530

    Add validation for different fields

--- backend/routes/createUser.cjs
+++ backend/routes/createUser.cjs
@@ -1,20 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User.cjs");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createuser", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-      location: req.body.location,
-    });
-    res.json({ success: true });
-  } catch (error) {
-    console.log("Error : " + error.message);
-    res.json({ success: false });
+router.post(
+  "/createuser",
+  [
+    body("email", "Given email is not in proper format.").isEmail(),
+    body("name", "Name length is too short.").isLength({ min: 5 }),
+    body("password", "Password length is too short.").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log("Error : " + error.message);
+      res.json({ success: false });
+    }
   }
-});
+);
 
 module.exports = router;
