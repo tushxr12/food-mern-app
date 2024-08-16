45a525cb8cd246cfa0722b3921b7fb9851016e40
tushxr12 <tusharforcourses@gmail.com>
2024-08-16 14:55:49 +0530

    Add route for createuser

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Signup from "./screens/Signup.js";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
+            <Route exact path="/createuser" element={<Signup />} />
           </Routes>
         </div>
       </Router>
