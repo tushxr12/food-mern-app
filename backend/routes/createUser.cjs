9f26fe798d62e14d8bb4ec532a8e6a0b31c64555
tushxr12 <tusharforcourses@gmail.com>
2024-08-18 15:11:31 +0530

    Add bcrypt while signup and login

--- backend/routes/createUser.cjs
+++ backend/routes/createUser.cjs
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.cjs");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+
+const jwtSecret = process.env.JWTSecret;
 
 router.post(
   "/createuser",
@@ -15,10 +20,14 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     try {
       await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: hashedPassword,
         email: req.body.email,
         location: req.body.location,
       });
@@ -49,12 +58,25 @@ router.post(
           .status(400)
           .json({ errors: "Email/Password is not correct." });
       }
-      if (req.body.password !== userData.password) {
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+      if (!pwdCompare) {
         return res
           .status(400)
           .json({ errors: "Email/Password is not correct." });
       }
-      res.json({ success: true });
+
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+
+      res.json({ success: true, authToken: authToken });
     } catch (error) {
       console.log("Error : " + error.message);
       res.json({ success: false });
