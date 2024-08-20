41ff5bc8602a7086f18678627da14d346ef101ed
tushxr12 <tusharforcourses@gmail.com>
2024-08-20 22:31:48 +0530

    create displayData route

---
+++ backend/routes/displayData.cjs
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/foodData", (req, res) => {
+  try {
+    res.send([global.food_items]);
+  } catch (error) {
+    console.error(error);
+    res.send("Server error: " + error);
+  }
+});
+
+module.exports = router;
