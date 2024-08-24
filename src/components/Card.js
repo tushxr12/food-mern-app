c78ed431ed3f5c2265af685fe691f7121e4d0b6f
tushxr12 <tusharforcourses@gmail.com>
2024-08-24 16:38:46 +0530

    Access data using props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  let options = props.options;
+  let priceOptions = Object.keys(options);
+
   return (
     <div>
       <div>
@@ -10,12 +13,11 @@ export default function Card() {
         >
           <img
             className="card-img-top"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyEFHFMDhEKtDxUKxhGm509XsgmjilIKTaKg&s"
+            src={props.imgSrc}
             alt="Card image cap"
           />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is some random text.</p>
+            <h5 className="card-title">{props.foodName}</h5>
             <div className="container w-100">
               <select name="" className="m-2 h-100 bg-success rounded" id="">
                 {Array.from(Array(6), (e, i) => {
@@ -27,8 +29,13 @@ export default function Card() {
                 })}
               </select>
               <select name="" className="m-2 h-100 bg-success rounded" id="">
-                <option value="half">Half</option>
-                <option value="full">Full</option>
+                {priceOptions.map((data) => {
+                  return (
+                    <option key={data} value={data}>
+                      {data}
+                    </option>
+                  );
+                })}
               </select>
 
               <div className="d-inline h-100 fs-5">Total Price:</div>
