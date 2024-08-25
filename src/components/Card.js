45cc8bfd53b56e82475c50a825559504fde06e1b
tushxr12 <tusharforcourses@gmail.com>
2024-08-26 00:54:35 +0530

    Add update logic in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,34 @@ export default function Card(props) {
   const [size, setSize] = useState("");
 
   const handleAddToCart = async () => {
+    let food = [];
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item;
+        break;
+      }
+    }
+
+    if (food !== []) {
+      if (food.size === size) {
+        await dispatch({
+          type: "UPDATE",
+          id: props.foodItem._id,
+          price: finalPrice,
+          qty: qty,
+        });
+      } else if (food.size !== size) {
+        await dispatch({
+          type: "ADD",
+          id: props.foodItem._id,
+          name: props.foodItem.name,
+          price: finalPrice,
+          qty: qty,
+          size: size,
+        });
+      }
+      return;
+    }
     await dispatch({
       type: "ADD",
       id: props.foodItem._id,
@@ -20,7 +48,7 @@ export default function Card(props) {
       qty: qty,
       size: size,
     });
-    console.log(data);
+    // console.log(data);
   };
   let finalPrice = qty * parseInt(options[size]);
   useEffect(() => {
@@ -67,7 +95,7 @@ export default function Card(props) {
               })}
             </select>
 
-            <div className="d-inline h-100 fs-5">Rs{finalPrice}/-</div>
+            <div className="d-inline h-100 fs-5">Rs. {finalPrice}/-</div>
           </div>
           <hr></hr>
           <button
