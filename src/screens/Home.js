d998ec5bd07c52a303583e5a7b670b67eb01345f
tushxr12 <tusharforcourses@gmail.com>
2024-08-25 16:19:05 +0530

    sebd entire item instead of single prop

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -133,9 +133,8 @@ export default function Home() {
                             className="col-12 col-md-6 col-lg-3"
                           >
                             <Card
-                              foodName={filterItems.name}
+                              foodItem={filterItems}
                               options={filterItems.options[0]}
-                              imgSrc={filterItems.img}
                             ></Card>
                           </div>
                         );
