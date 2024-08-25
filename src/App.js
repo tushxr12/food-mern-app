2e5714f2a4a97f115d68b1d16fbd026cc2d2ce7d
tushxr12 <tusharforcourses@gmail.com>
2024-08-25 16:19:59 +0530

    Wrapping app inside context Reducer

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Login from "./screens/Login";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Signup from "./screens/Signup.js";
+import { CartProvider } from "./components/ContextReducer.js";
 
 function App() {
   return (
     <>
-      <Router>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/createuser" element={<Signup />} />
-          </Routes>
-        </div>
-      </Router>
+      <CartProvider>
+        <Router>
+          <div>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/createuser" element={<Signup />} />
+            </Routes>
+          </div>
+        </Router>
+      </CartProvider>
     </>
   );
 }
