0c1970e0e0353e6e3ee1abdadab297232dcc8b76
tushxr12 <tusharforcourses@gmail.com>
2024-08-26 14:56:00 +0530

    Store userEmail in local storage

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -28,6 +28,7 @@ export default function Login() {
     }
 
     if (json.success) {
+      localStorage.setItem("userEmail", credentials.email);
       localStorage.setItem("authToken", json.authToken);
       navigate("/");
     }
