048a62382ace56a776dd50ab6af8ff93288b19a6
tushxr12 <tusharforcourses@gmail.com>
2024-08-26 14:52:44 +0530

    create orders model

---
+++ backend/models/Orders.cjs
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  order_data: {
+    type: Array,
+    required: true,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
